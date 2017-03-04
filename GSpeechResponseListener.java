9e1a0fbe1ce737ae7a13cdf216f751f2b532a51c
Vaishali-56 <vvaishali1116@outlook.com>
2017-03-04 20:31:38 +0530

    Add files via upload

---
+++ GSpeechResponseListener.java
@@ -0,0 +1,12 @@
+package com.darkprograms.speech.recognizer;
+/**
+ * Response listeners for URL connections.
+ *
+ *
+ */
+
+public interface GSpeechResponseListener {
+	
+	public void onResponse(GoogleResponse gr);
+	
+}
